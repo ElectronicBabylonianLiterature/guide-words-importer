2455c64c2b53de6b86d1d7103270f8f06a8e6ef8
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-01-17 10:39:36 +0100

    Load guide words from file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,15 @@
+import * as fs from 'fs'
 import {Command, flags} from '@oclif/command'
 import {MongoClient} from 'mongodb'
+import * as Papa from 'papaparse'
 
+interface GuideWords {
+  lemma: string;
+  eblHomonym: string;
+  eblGuideWord: string;
+  oraccLemma: string;
+  oraccGuideWord: string;
+}
 class GuideWordsImporter extends Command {
   static description = 'describe the command here'
 
@@ -10,23 +19,54 @@ class GuideWordsImporter extends Command {
     host: flags.string({char: 'h', description: 'MongoDB URI'}),
   }
 
-  static args = [{name: 'file'}]
+  static args = [{name: 'file', required: true}]
 
   async run() {
-    const {args, flags} = this.parse(GuideWordsImporter)
+    const {args: {file}, flags} = this.parse(GuideWordsImporter)
 
     const uri = flags.host || 'mongodb://localhost:27017'
+
+    const guideWords = await this.loadGuideWords(file)
+    await this.updateFragments(uri, guideWords)
+  }
+
+  private async loadGuideWords(file: string): Promise<readonly GuideWords[]> {
+    this.log(`Loading guide words from ${file}...`)
+    const csv = await new Promise<string>((resolve, reject) => {
+      fs.readFile(file, 'utf8', (err, data) =>
+        err ? reject(err) : resolve(data))
+    })
+    return Papa.parse(csv, {
+      header: true,
+    }).data
+  }
+
+  private async updateFragments(uri: string, guideWords: readonly GuideWords[]) {
     this.log(`Connecting to MongoDB ${uri}...`)
-
-    if (args.file) {
-      this.log(`you input --file: ${args.file}`)
-    }
-
     const client = new MongoClient(uri, {useNewUrlParser: true})
+
+    const operations = guideWords.map(guideWord => ({
+      updateOne: {
+        filter: {
+          lemma: [guideWord.lemma],
+          homonym: guideWord.eblHomonym,
+        },
+        update: {$set: {
+          guideWord: guideWord.eblGuideWord,
+          oraccWords: [
+            {
+              lemma: guideWord.oraccLemma,
+              guideWord: guideWord.oraccGuideWord,
+            },
+          ],
+        }},
+      },
+    }))
+
     try {
       await client.connect()
       const collection = client.db('ebl').collection('fragments')
-      await collection.updateOne({_id: 'K.1'}, {$set: {guideWord: '', oraccWords: []}})
+      await collection.bulkWrite(operations)
       await client.close()
     } catch (error) {
       this.error(error)
--- test/index.test.ts
+++ test/index.test.ts
@@ -6,10 +6,16 @@ import cmd = require('../src')
 
 describe('guide-words-importer', () => {
   test
-  .stdout()
+  .stderr()
   .do(() => cmd.run([]))
   .exit(2)
-  .it('runs importgws', ctx => {
+  .it('missing file argument')
+
+  test
+  .stdout()
+  .do(() => cmd.run(['./test/guide-words.csv']))
+  .exit(2)
+  .it('default database', ctx => {
     expect(ctx.stdout).to.contain('mongodb://localhost:27017')
   })
 
@@ -21,12 +27,32 @@ describe('guide-words-importer', () => {
   .do(async ctx => ctx.client.connect())
   .finally(async ctx => ctx.client.close())
   .add('collection', ctx => ctx.client.db('ebl').collection('fragments'))
-  .do(async ctx => ctx.collection.insertOne({_id: 'K.1'}))
+  .do(async ctx => ctx.collection.insertOne({_id: 'X.1', homonym: 'I', lemma: ['ebllemma1']}))
+  .do(async ctx => ctx.collection.insertOne({_id: 'X.2', homonym: 'I', lemma: ['ebllemma2']}))
   .stdout()
-  .do(ctx => cmd.run(['--host', ctx.uri]))
-  .it('runs importgws --host ...', async ctx => {
+  .do(ctx => cmd.run(['--host', ctx.uri, './test/guide-words.csv']))
+  .it('imports guide words', async ctx => {
     expect(ctx.stdout).to.contain(ctx.uri)
-    const fragment = await ctx.collection.findOne({_id: 'K.1'})
-    expect(fragment).to.deep.equal({_id: 'K.1', guideWord: '', oraccWords: []})
+    const fragments = await ctx.collection.find().toArray()
+    expect(fragments).to.deep.equal([
+      {
+        _id: 'X.1',
+        lemma: ['ebllemma1'],
+        homonym: 'I',
+        guideWord: 'ebl gw1',
+        oraccWords: [
+          {lemma: 'oracclemma1', guideWord: 'oracc gw1'},
+        ],
+      },
+      {
+        _id: 'X.2',
+        lemma: ['ebllemma2'],
+        homonym: 'I',
+        guideWord: 'ebl gw2',
+        oraccWords: [
+          {lemma: 'oracclemma2', guideWord: 'oracc gw2'},
+        ],
+      },
+    ])
   })
 })
