7053e0d454fe3d354fdd9b94d306191e7131a78c
Jussi Laasonen <jussi.laasonen@lmu.de>
2020-01-17 11:28:14 +0100

    Add arg default and descriptions to the command

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,18 @@ import {updateFragments} from './fragment-repository'
 import {parseFromFile} from './guide-word-parser'
 
 class GuideWordsImporter extends Command {
-  static description = 'describe the command here'
+  static description = 'Imports guide words from a CSV file to MongoDB.'
 
   static flags = {
     version: flags.version({char: 'v'}),
     help: flags.help({char: 'h'}),
-    host: flags.string({char: 'h', description: 'MongoDB URI'}),
+    host: flags.string({char: 'h', description: 'MongoDB URI', default: 'mongodb://localhost:27017'}),
   }
 
-  static args = [{name: 'file', required: true}]
+  static args = [{name: 'file', description: 'Path to the CSV file', required: true}]
 
   async run() {
-    const {args: {file}, flags: {host = 'mongodb://localhost:27017'}} = this.parse(GuideWordsImporter)
+    const {args: {file}, flags: {host}} = this.parse(GuideWordsImporter)
 
     try {
       this.log(`Loading guide words from ${file}...`)
